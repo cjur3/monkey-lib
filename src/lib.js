6266eb16bf5b2264e0c2291b10c65e1ff40e14f6
cjur3 <cjure89@gmail.com>
2026-04-29 18:31:04 +0200

    Initial commit

---
+++ src/lib.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import * as MaterialUI from "@mui/material";
+import * as ReactFlow from "@xyflow/react";
+
+export { React, ReactDOM, MaterialUI, ReactFlow };
+export default { React, ReactDOM, MaterialUI, ReactFlow };
